6e30f883054fa13d188e45f614f4ae51a2b9ec0a
Phung Vinh Khang <pvk@netcompany.com>
2025-05-30 20:22:54 +0700

    feat: dagster to kafka

--- pipeline/README.md
+++ pipeline/README.md
@@ -1,7 +1,6 @@
 ```shell
 pip install -e ".[dev]"
-$env:DAGSTER_HOME=$(pwd)
-
+docker compose up -d
 cd pipeline
 http://127.0.0.1:3000
 ```
--- pipeline/repository.py
+++
@@ -1,6 +0,0 @@
-from dagster import repository
-from jobs import weather_job, weather_schedule
-
-@repository
-def weather_repo():
-    return [weather_job, weather_schedule]
